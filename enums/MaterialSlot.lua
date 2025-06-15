a944bc05c76432f340d11791e5f5e3be9c958069
John <414568+johnfking@users.noreply.github.com>
2025-06-15 10:28:27 -0400

    feature: adds new enum for botdyearmor MateralSlots

---
+++ enums/MaterialSlot.lua
@@ -0,0 +1,12 @@
+---@enum (key) MaterialSlot
+MaterialSlot = {
+    ALL    = '*',
+    HEAD   = '1',
+    CHEST  = '2',
+    ARMS   = '3',
+    WRISTS = '4',
+    HANDS  = '5',
+    LEGS   = '6',
+    FEET   = '7',
+}
+return MaterialSlot
