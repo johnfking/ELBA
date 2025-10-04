82813f265867510aed2b6636e91c3a9cd4f41407
John <king.johnf@gmail.com>
2025-10-04 06:20:02 -0400

    Add SpellDelayCategory enum and adopt it for spelldelays

---
+++ enums/SpellHoldCategory.lua
@@ -0,0 +1,28 @@
+---@enum (key) SpellHoldCategory
+SpellHoldCategory = {
+    NUKES                = 'nukes',
+    REGULAR_HEALS        = 'regularheals',
+    ROOTS                = 'roots',
+    PETS                 = 'pets',
+    SNARES               = 'snares',
+    DOTS                 = 'dots',
+    SLOWS                = 'slows',
+    DEBUFFS              = 'debuffs',
+    CURES                = 'cures',
+    STUNS                = 'stuns',
+    COMPLETE_HEALS       = 'completeheals',
+    GROUP_HEALS          = 'groupheals',
+    GROUP_COMPLETE_HEALS = 'groupcompleteheals',
+    GROUP_HOT_HEALS      = 'grouphotheals',
+    HOT_HEALS            = 'hotheals',
+    AE_DEBUFFS           = 'aedebuffs',
+    AE_SLOWS             = 'aeslows',
+    PET_REGULAR_HEALS    = 'petregularheals',
+    PET_HOT_HEALS        = 'pethotheals',
+    PET_COMPLETE_HEALS   = 'petcompleteheals',
+    PET_CURES            = 'petcures',
+    PET_DAMAGE_SHIELDS   = 'petdamageshields',
+    PET_RESIST_BUFFS     = 'petresistbuffs',
+}
+
+return SpellHoldCategory
