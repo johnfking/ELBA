eb021d3e5f1bd16d144350d680aea9f0fa1b8ac2
John <414568+johnfking@users.noreply.github.com>
2026-03-08 10:26:36 -0400

    Convert property.lua annotations to LuaCATS format
    - Changed @param to ---@param
    - Changed @return to ---@return
    - Added proper nil type annotations (|nil) for optional parameters
    - Improves language server type checking and autocomplete

--- spec/property.lua
+++ spec/property.lua
@@ -13,9 +13,9 @@ math.randomseed(rng_seed)
 property._seed = rng_seed
 
 --- Generate random integers within a range
--- @param min number minimum value (inclusive)
--- @param max number maximum value (inclusive)
--- @return table generator that produces random integers
+---@param min number minimum value (inclusive)
+---@param max number maximum value (inclusive)
+---@return table generator that produces random integers
 function property.integer(min, max)
   assert(type(min) == 'number', 'min must be a number')
   assert(type(max) == 'number', 'max must be a number')
@@ -29,7 +29,7 @@ function property.integer(min, max)
 end
 
 --- Generate random boolean values
--- @return table generator that produces true or false
+---@return table generator that produces true or false
 function property.boolean()
   return {
     generate = function()
@@ -39,10 +39,10 @@ function property.boolean()
 end
 
 --- Generate random strings
--- @param len_min number minimum length
--- @param len_max number maximum length
--- @param charset string optional character set (default: alphanumeric)
--- @return table generator that produces random strings
+---@param len_min number minimum length
+---@param len_max number maximum length
+---@param charset string|nil optional character set (default: alphanumeric)
+---@return table generator that produces random strings
 function property.string(len_min, len_max, charset)
   assert(type(len_min) == 'number', 'len_min must be a number')
   assert(type(len_max) == 'number', 'len_max must be a number')
@@ -65,8 +65,8 @@ function property.string(len_min, len_max, charset)
 end
 
 --- Generate random selection from a list
--- @param list table list of values to select from
--- @return table generator that produces random selections
+---@param list table list of values to select from
+---@return table generator that produces random selections
 function property.oneof(list)
   assert(type(list) == 'table', 'list must be a table')
   assert(#list > 0, 'list must not be empty')
@@ -80,9 +80,9 @@ function property.oneof(list)
 end
 
 --- Run a property test with random inputs
--- @param generators table list of generators to produce test inputs
--- @param test_fn function test function that receives generated values
--- @param opts table optional configuration (iterations, seed)
+---@param generators table list of generators to produce test inputs
+---@param test_fn function test function that receives generated values
+---@param opts table|nil optional configuration (iterations, seed)
 function property.forall(generators, test_fn, opts)
   assert(type(generators) == 'table', 'generators must be a table')
   assert(type(test_fn) == 'function', 'test_fn must be a function')
@@ -120,9 +120,9 @@ function property.forall(generators, test_fn, opts)
 end
 
 --- Generate sample values from a generator for debugging
--- @param generator table generator to sample from
--- @param count number number of samples to generate (default: 10)
--- @return table list of generated sample values
+---@param generator table generator to sample from
+---@param count number|nil number of samples to generate (default: 10)
+---@return table list of generated sample values
 function property.sample(generator, count)
   assert(type(generator) == 'table' and type(generator.generate) == 'function',
     'generator must have a generate function')
